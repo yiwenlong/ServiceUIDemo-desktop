86172de42f87f756d04e978147bd7dc6e407d2ca
yiwenlong <wlong.yi@gmail.com>
2020-04-26 17:05:39 +0800

    Support config server process as a windows service.

---
+++ helper/config-server.go
@@ -0,0 +1,39 @@
+package helper
+
+// document: https://github.com/winsw/winsw
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type Service struct {
+	XMLName          xml.Name `xml:"service"`
+	Id               string   `xml:"id"`
+	Name             string   `xml:"name"`
+	Description      string   `xml:"description"`
+	Executable       string   `xml:"executable"`
+	WorkingDirectory string   `xml:"workingdirectory"`
+	Argument         []string `xml:"argument"`
+	Logmode          string   `xml:"logmode"`
+}
+
+func ConfigServer(homeDir, executable string, args ...string) error {
+	service := Service{
+		Id:               "MyDemoServer",
+		Name:             "MyDemoServer",
+		Description:      "A demo service run on Windows operation system.",
+		Executable:       executable,
+		Logmode:          "rotate",
+		WorkingDirectory: homeDir,
+	}
+	for _, arg := range args {
+		service.Argument = append(service.Argument, arg)
+	}
+	bs, err := xml.MarshalIndent(service, "", "    ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filepath.Join(homeDir, "service.xml"), bs, 0666)
+}
---
+++ helper/config-server_test.go
@@ -0,0 +1,23 @@
+package helper
+
+import "testing"
+
+func TestConfigServer(t *testing.T) {
+	type args struct {
+		homeDir string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"demo", args{"D:\\gopath\\src\\github.com\\yiwenlong\\launchduidemo\\windows"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ConfigServer(tt.args.homeDir); (err != nil) != tt.wantErr {
+				t.Errorf("ConfigServer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
